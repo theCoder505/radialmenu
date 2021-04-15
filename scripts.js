509d0a734919e58150c4f1510b9d8e91def1d310
A.H.K. EMAD <68850204+theCoder505@users.noreply.github.com>
2021-04-16 03:04:36 +0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,303 @@
+var shared = document.querySelector(".SHARED");
+var shared2 = document.querySelector(".SHARED2");
+var cabin = document.querySelector(".CABIN");
+var cabin2 = document.querySelector(".CABIN2");
+var sed = document.querySelector(".SED");
+var sed1 = document.querySelector(".SED1");
+var sed2 = document.querySelector(".SED2");
+var sep = document.querySelector(".SEP");
+var sep1 = document.querySelector(".SEP1");
+var sep2 = document.querySelector(".SEP2");
+var sao = document.querySelector(".SA0");
+var sao1 = document.querySelector(".SA01");
+var sao2 = document.querySelector(".SA02");
+var sao3 = document.querySelector(".SA03");
+var seats = document.querySelector(".SEATS");
+var seats1 = document.querySelector(".SEATS1");
+var seats2 = document.querySelector(".SEATS2");
+var seats3 = document.querySelector(".SEATS3");
+
+
+shared.addEventListener("click", function () {
+
+    shared.style.backgroundColor = shared.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+    shared2.style.backgroundColor = shared2.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+  
+
+});
+
+shared2.addEventListener("click", function () {
+
+    shared.style.backgroundColor = shared.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+    shared2.style.backgroundColor = shared2.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+  
+
+});
+
+document.querySelector(".menunav .coconut").addEventListener("click", function () {
+
+    shared.style.backgroundColor = shared.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+    shared2.style.backgroundColor = shared2.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+  
+
+});
+
+shared.addEventListener("click", function () {
+
+    shared.style.backgroundColor = shared.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+    shared2.style.backgroundColor = shared2.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+ 
+});
+
+
+
+
+// -----------------------------------------------------
+
+cabin.addEventListener("click", function () {
+
+    cabin.style.backgroundColor = cabin.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+    cabin2.style.backgroundColor = cabin2.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+   
+    
+});
+document.querySelector(".menunav .cherry").addEventListener("click", function () {
+
+    cabin.style.backgroundColor = cabin.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+    cabin2.style.backgroundColor = cabin2.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+
+});
+
+cabin2.addEventListener("click", function () {
+
+    cabin.style.backgroundColor = cabin.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+    cabin2.style.backgroundColor = cabin2.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+   
+    
+});
+cabin.addEventListener("click", function () {
+
+    cabin.style.backgroundColor = cabin.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+    cabin2.style.backgroundColor = cabin2.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+   
+    
+});
+// -----------------------------------------------------
+
+
+sed.addEventListener("click", function () {
+
+    sed.style.backgroundColor = sed.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+    sed1.style.backgroundColor = sed1.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    sed2.style.backgroundColor = sed2.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+   
+});
+
+document.querySelector(".menunav .blackberry").addEventListener("click", function () {
+
+    sed.style.backgroundColor = sed.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+    sed1.style.backgroundColor = sed1.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    sed2.style.backgroundColor = sed2.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+
+});
+
+sed1.addEventListener("click", function () {
+
+    sed.style.backgroundColor = sed.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+    sed1.style.backgroundColor = sed1.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    sed2.style.backgroundColor = sed2.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+   
+});
+sed2.addEventListener("click", function () {
+
+    sed.style.backgroundColor = sed.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+    sed1.style.backgroundColor = sed1.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    sed2.style.backgroundColor = sed2.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+   
+});
+
+
+sed.addEventListener("click", function () {
+
+    sed.style.backgroundColor = sed.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+    sed1.style.backgroundColor = sed1.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    sed2.style.backgroundColor = sed2.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+   
+});
+// -----------------------------------------------------
+
+sep.addEventListener("click", function () {
+
+    sep.style.backgroundColor = sep.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+    sep1.style.backgroundColor = sep1.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    sep2.style.backgroundColor = sep2.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+   
+ 
+});
+document.querySelector(".menunav .grape").addEventListener("click", function () {
+
+    sep.style.backgroundColor = sep.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+    sep1.style.backgroundColor = sep1.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    sep2.style.backgroundColor = sep2.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+   
+ 
+});
+sep1.addEventListener("click", function () {
+
+    sep.style.backgroundColor = sep.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+    sep1.style.backgroundColor = sep1.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    sep2.style.backgroundColor = sep2.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+   
+ 
+});
+sep2.addEventListener("click", function () {
+
+    sep.style.backgroundColor = sep.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+    sep1.style.backgroundColor = sep1.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    sep2.style.backgroundColor = sep2.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+   
+ 
+});
+
+sep.addEventListener("click", function () {
+
+    sep.style.backgroundColor = sep.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+    sep1.style.backgroundColor = sep1.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    sep2.style.backgroundColor = sep2.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+   
+ 
+});
+// -----------------------------------------------------
+
+sao.addEventListener("click", function () {
+
+    sao.style.backgroundColor = sao.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+    sao1.style.backgroundColor = sao1.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    sao2.style.backgroundColor = sao2.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    sao3.style.backgroundColor = sao3.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+   
+
+});
+
+document.querySelector(".menunav .almond").addEventListener("click", function(){
+    // alert("clicked");
+    sao.style.backgroundColor = sao.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+    sao1.style.backgroundColor = sao1.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    sao2.style.backgroundColor = sao2.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    sao3.style.backgroundColor = sao3.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+   
+});
+
+sao1.addEventListener("click", function(){
+    // alert("clicked cst");
+    sao.style.backgroundColor = sao.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+    sao1.style.backgroundColor = sao1.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    sao2.style.backgroundColor = sao2.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    sao3.style.backgroundColor = sao3.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+   
+});
+sao2.addEventListener("click", function(){
+    // alert("clicked cst");
+    sao.style.backgroundColor = sao.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+    sao1.style.backgroundColor = sao1.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    sao2.style.backgroundColor = sao2.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    sao3.style.backgroundColor = sao3.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+   
+});
+sao3.addEventListener("click", function(){
+    // alert("clicked cst");
+    sao.style.backgroundColor = sao.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+    sao1.style.backgroundColor = sao1.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    sao2.style.backgroundColor = sao2.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    sao3.style.backgroundColor = sao3.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+   
+});
+sao.addEventListener("click", function () {
+
+    sao.style.backgroundColor = sao.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+    sao1.style.backgroundColor = sao1.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    sao2.style.backgroundColor = sao2.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    sao3.style.backgroundColor = sao3.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+   
+
+});
+
+
+seats.addEventListener("click", function () {
+
+    seats.style.backgroundColor = seats.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+    seats1.style.backgroundColor = seats1.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    seats2.style.backgroundColor = seats2.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    seats3.style.backgroundColor = seats3.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+   
+    
+});
+
+
+// -----------------------------------------------------
+
+
+// -----------------------------------------------------
+// -----------------------------------------------------
+
+document.querySelector(".safran").addEventListener("click", function(){
+    shared.style.backgroundColor = shared.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+    shared2.style.backgroundColor = shared2.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+    cabin.style.backgroundColor = cabin.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+    cabin2.style.backgroundColor = cabin2.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    sed.style.backgroundColor = sed.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+    sed1.style.backgroundColor = sed1.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    sed2.style.backgroundColor = sed2.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    sep.style.backgroundColor = sep.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+    sep1.style.backgroundColor = sep1.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    sep2.style.backgroundColor = sep2.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    sao.style.backgroundColor = sao.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+    sao1.style.backgroundColor = sao1.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    sao2.style.backgroundColor = sao2.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    sao3.style.backgroundColor = sao3.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    seats.style.backgroundColor = seats.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+    seats1.style.backgroundColor = seats1.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    seats2.style.backgroundColor = seats2.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    seats3.style.backgroundColor = seats3.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+ 
+});
+
+// -----------------------------------------------------
+// -----------------------------------------------------
+
+seats1.addEventListener("click", function () {
+    // alert("hello");
+    seats.style.backgroundColor = seats.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    seats1.style.backgroundColor = seats1.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    seats2.style.backgroundColor = seats2.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    seats3.style.backgroundColor = seats3.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+   
+});
+
+
+document.querySelector(".menunav .vanilla").addEventListener("click", function(){
+    // alert("clicked");
+    seats.style.backgroundColor = seats.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+    seats1.style.backgroundColor = seats1.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    seats2.style.backgroundColor = seats2.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    seats3.style.backgroundColor = seats3.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+   
+});
+
+document.querySelector(".SEATS3").addEventListener("click", function(){
+    // alert("clicked cst");
+    seats.style.backgroundColor = seats.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+    seats1.style.backgroundColor = seats1.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    seats2.style.backgroundColor = seats2.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    seats3.style.backgroundColor = seats3.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+   
+});
+seats.addEventListener("click", function(){
+    seats.style.backgroundColor = seats.style.backgroundColor === "lightgreen" ? "#1da5c3" : "lightgreen";
+    seats1.style.backgroundColor = seats1.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    seats2.style.backgroundColor = seats2.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+    seats3.style.backgroundColor = seats3.style.backgroundColor === "lightgreen" ? "#27D2AB" : "lightgreen";
+   
+});
+
+// 1366 = width 
